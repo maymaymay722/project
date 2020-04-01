1af71c564b8a53586299432ff04173220340a4f4
maymaymay722 <meiyingying0722@163.com>
2020-04-01 21:15:09 +0800

    唐诗可视化web

---
+++ 唐诗可视化/src/com/bit/DBUtil.java
@@ -0,0 +1,35 @@
+
+//package com.bit;
+//
+//import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+//
+//import javax.sql.DataSource;
+//import java.sql.Connection;
+//import java.sql.SQLException;
+//
+//public class DBConfig {
+//    private static final String host = "127.0.0.1";
+//    private static final int port = 3306;
+//    private static final String user = "root";
+//    private static final String password = "1234";
+//    private static final String database = "tangshi";
+//
+//    // 用的饿汉模式的单例
+//    private static final DataSource dataSource;
+//    static {
+//        MysqlDataSource mysqlDataSource = new MysqlDataSource();
+//        mysqlDataSource.setServerName(host);
+//        mysqlDataSource.setPort(port);
+//        mysqlDataSource.setUser(user);
+//        mysqlDataSource.setPassword(password);
+//        mysqlDataSource.setDatabaseName(database);
+//        mysqlDataSource.setUseSSL(false);
+//        mysqlDataSource.setCharacterEncoding("utf8");
+//
+//        dataSource = mysqlDataSource;
+//    }
+//
+//    public static Connection getConnection() throws SQLException {
+//        return dataSource.getConnection();
+//    }
+//}
